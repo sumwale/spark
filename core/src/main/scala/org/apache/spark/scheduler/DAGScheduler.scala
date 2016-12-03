7e17d6040354b2b89cb2eddd4d3dca8fd31b5177
Sumedh Wale <swale@snappydata.io>
2016-12-03 17:37:31 +0530

    [SNAP-1202] Reduce serialization overheads of biggest contributors in queries (#34)
    - Cloning of properties uses SerializationUtils which is inefficient. Instead added
      Utils.cloneProperties that will clone by walking all its entries (including defaults if requested)
    - Separate out WholeStageCodegenExec closure invocation into its own WholeStageCodegenRDD
      for optimal serialization of its components including base RDD and CodeAndComment.
      This RDD also removes the limitation of having a max of only 2 RDDs in inputRDDs().

--- core/src/main/scala/org/apache/spark/scheduler/DAGScheduler.scala
+++ core/src/main/scala/org/apache/spark/scheduler/DAGScheduler.scala
@@ -31,8 +31,6 @@ import scala.language.existentials
 import scala.language.postfixOps
 import scala.util.control.NonFatal
 
-import org.apache.commons.lang3.SerializationUtils
-
 import org.apache.spark._
 import org.apache.spark.broadcast.Broadcast
 import org.apache.spark.executor.TaskMetrics
@@ -720,7 +718,7 @@ private[spark] class DAGScheduler(
     val waiter = new JobWaiter(this, jobId, partitions.size, resultHandler)
     eventProcessLoop.post(JobSubmitted(
       jobId, rdd, func2, partitions.toArray, callSite, waiter,
-      SerializationUtils.clone(properties)))
+      Utils.cloneProperties(properties)))
     waiter
   }
 
@@ -785,7 +783,7 @@ private[spark] class DAGScheduler(
     val partitions = (0 until rdd.partitions.length).toArray
     val jobId = nextJobId.getAndIncrement()
     eventProcessLoop.post(JobSubmitted(
-      jobId, rdd, func2, partitions, callSite, listener, SerializationUtils.clone(properties)))
+      jobId, rdd, func2, partitions, callSite, listener, Utils.cloneProperties(properties)))
     listener.awaitResult()    // Will throw an exception if the job fails
   }
 
@@ -820,7 +818,7 @@ private[spark] class DAGScheduler(
     // the map output tracker and some node failures had caused the output statistics to be lost.
     val waiter = new JobWaiter(this, jobId, 1, (i: Int, r: MapOutputStatistics) => callback(r))
     eventProcessLoop.post(MapStageSubmitted(
-      jobId, dependency, callSite, waiter, SerializationUtils.clone(properties)))
+      jobId, dependency, callSite, waiter, Utils.cloneProperties(properties)))
     waiter
   }
 
