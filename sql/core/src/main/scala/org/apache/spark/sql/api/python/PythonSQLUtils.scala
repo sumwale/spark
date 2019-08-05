b57a5c033867059d71544e674b6edf0059573f01
Sumedh Wale <swale@tibco.com>
2019-08-05 21:07:57 +0530

    [SNAPPYDATA] add SnappyData builtin functions by reflection (#169)
    The function descriptions loaded by reflection are used for SQL documentation generation by
    PythonSQLUtils.

--- sql/core/src/main/scala/org/apache/spark/sql/api/python/PythonSQLUtils.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/api/python/PythonSQLUtils.scala
@@ -14,12 +14,31 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+/*
+ * Changes for TIBCO ComputeDB data platform.
+ *
+ * Portions Copyright (c) 2017-2020 TIBCO Software Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License. You
+ * may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License. See accompanying
+ * LICENSE file.
+ */
 
 package org.apache.spark.sql.api.python
 
 import java.io.InputStream
 import java.nio.channels.Channels
 
+import org.apache.spark.SparkSnappyUtils
 import org.apache.spark.api.java.JavaRDD
 import org.apache.spark.api.python.PythonRDDServer
 import org.apache.spark.rdd.RDD
@@ -35,7 +54,11 @@ private[sql] object PythonSQLUtils {
 
   // This is needed when generating SQL documentation for built-in functions.
   def listBuiltinFunctionInfos(): Array[ExpressionInfo] = {
-    FunctionRegistry.functionSet.flatMap(f => FunctionRegistry.builtin.lookupFunction(f)).toArray
+    // noinspection ConvertibleToMethodValue
+    val base = FunctionRegistry.functionSet.flatMap(FunctionRegistry.builtin.lookupFunction(_))
+    val extra = SparkSnappyUtils.additionalBuiltinFunctions
+        .asInstanceOf[Seq[(String, ExpressionInfo, Any)]].map(_._2)
+    if (extra.nonEmpty) (base ++ extra).toArray else base.toArray
   }
 
   /**
