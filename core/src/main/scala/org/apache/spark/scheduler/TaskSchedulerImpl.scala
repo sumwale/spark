840a4b3fbc6810156f04af9d0349ba207e8b4928
suranjan kumar <skumar@snappydata.io>
2019-12-06 10:47:28 +0530

    Change the maxTaskFailures depending on property (#186)
    * Change the maxTaskFailures depending on property
    
    The local propery is set based on plan from snappy side
    If the propery is set, then maxTaskFailures is set to the set value

--- core/src/main/scala/org/apache/spark/scheduler/TaskSchedulerImpl.scala
+++ core/src/main/scala/org/apache/spark/scheduler/TaskSchedulerImpl.scala
@@ -82,6 +82,8 @@ private[spark] class TaskSchedulerImpl(
   // How often to check for speculative tasks
   val SPECULATION_INTERVAL_MS = conf.getTimeAsMs("spark.speculation.interval", "100ms")
 
+  val SNAPPY_WRITE_RETRY_PROP = "snappydata.maxRetryAttemptsForWrite"
+
   // Duplicate copies of a task will only be launched if the original copy has been running for
   // at least this amount of time. This is to avoid the overhead of launching speculative copies
   // of tasks that are very short.
@@ -194,7 +196,18 @@ private[spark] class TaskSchedulerImpl(
     val tasks = taskSet.tasks
     logInfo("Adding task set " + taskSet.id + " with " + tasks.length + " tasks")
     this.synchronized {
-      val manager = createTaskSetManager(taskSet, maxTaskFailures)
+      val maxRetryAttemptsForWrite = taskSet.properties.
+        getProperty(SNAPPY_WRITE_RETRY_PROP)
+
+      logInfo("The maxRetryAttemptsForWrite is set to " + maxRetryAttemptsForWrite +
+        "maxTaskFailure " + maxTaskFailures)
+      val maxRetryAttempts = if (maxRetryAttemptsForWrite != null) {
+        maxRetryAttemptsForWrite.toInt
+      } else {
+        maxTaskFailures
+      }
+
+      val manager = createTaskSetManager(taskSet, maxRetryAttempts)
       val stage = taskSet.stageId
       val stageTaskSets =
         taskSetsByStageIdAndAttempt.getOrElseUpdate(stage, new HashMap[Int, TaskSetManager])
